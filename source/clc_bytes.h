a19851ca23bf20439e3d7d91be627d80314c6ff6
sebsgit <s.baginski@mail.com>
2016-02-20 12:34:41 +0100

    removed printing functions

--- source/clc_bytes.h
+++ source/clc_bytes.h
@@ -13,11 +13,6 @@ typedef struct {
 	unsigned char b[20];
 } clc_bytes_20;
 
-extern void clc_print_b(const clc_bytes_16 * x);
-extern void clc_print_b_mat(const clc_bytes_16 * x);
-
-extern void clc_print_b_20(const clc_bytes_20 * x);
-
 #ifdef __cplusplus
 }
 #endif
--- source/main.c
+++ source/main.c
@@ -7,6 +7,33 @@
 #include "clc_encrypt.h"
 #include "clc_hash.h"
 
+void clc_print_b(const clc_bytes_16 * x){
+	short i=0;
+	for(i=0 ; i<16 ; ++i){
+		printf("%x ",x->b[i]);
+	}
+	printf("\n");
+}
+
+void clc_print_b_mat(const clc_bytes_16 * x){
+	short r=0, c=0;
+	for(r=0 ; r<4 ; ++r){
+		for(c=0 ; c<4 ; ++c){
+			printf("%x ",x->b[r + c * 4]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void clc_print_b_20(const clc_bytes_20 * x){
+	short i=0;
+	for(i=0 ; i<20 ; ++i){
+		printf("%x ",x->b[i]);
+	}
+	printf("\n");
+}
+
 static short clc_test_eq( const clc_bytes_16 * x1, const clc_bytes_16 * x2 ){
 	return (0 == memcmp(x1->b,x2->b,16));
 }
