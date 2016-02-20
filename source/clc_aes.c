023066944454227863209fc4b7a7901c168c93c3
sebsgit <s.baginski@mail.com>
2016-02-20 12:27:56 +0100

    cleanup

--- source/clc_aes.c
+++ source/clc_aes.c
@@ -16,17 +16,21 @@ static void clc_print_key_base(const unsigned char* key, const short size) {
 	printf("\n");
 }
 
+static void clc_init_key_base(unsigned char* key, const unsigned char* data, const int data_len, const short key_len) {
+	assert(data_len > 0 && data_len <= key_len && "FAIL: data length incorrect !");
+	clc_init_key(key,data,data_len);
+	if(data_len < key_len){
+		memset(key+data_len, 0, key_len-data_len );
+	}
+}
+
 void clc_print_key_16( const clc_aes_key_128 * key ){ clc_print_key_base(key->b, 176); }
 void clc_print_key_24( const clc_aes_key_192 * key ){ clc_print_key_base(key->b, 208); }
 void clc_print_key_32( const clc_aes_key_256 * key ){ clc_print_key_base(key->b, 240); }
 
-void clc_init_key_16( clc_aes_key_128 * key, const unsigned char * data, int data_len ){
-	assert(data_len>0 && data_len<=16 && "FAIL: data length must be [1,16] !");
-	clc_init_key(key->b,data,data_len);
-	if(data_len<16){
-		memset(key->b+data_len, 0, 16-data_len );
-	}
-}
+void clc_init_key_16( clc_aes_key_128 * key, const unsigned char * data, int data_len ){ clc_init_key_base(key->b, data, data_len, 16); }
+void clc_init_key_24( clc_aes_key_192 * key, const unsigned char * data, int data_len ){ clc_init_key_base(key->b, data, data_len, 24); }
+void clc_init_key_32( clc_aes_key_256 * key, const unsigned char * data, int data_len ){ clc_init_key_base(key->b, data, data_len, 32); }
 
 void clc_expand_key_16( clc_aes_key_128 * key ){
 	unsigned char buff[4];
@@ -46,15 +50,6 @@ void clc_expand_key_16( clc_aes_key_128 * key ){
 	}
 }
 
-
-void clc_init_key_24( clc_aes_key_192 * key, const unsigned char * data, int data_len ){
-	assert(data_len>0 && data_len<=24 && "FAIL: data length must be [1,24] !");
-	clc_init_key(key->b,data,data_len);
-	if(data_len<24){
-		memset(key->b+data_len, 0, 24-data_len );
-	}
-}
-
 void clc_expand_key_24( clc_aes_key_192 * key ){
 	unsigned char buff[4];
 	unsigned char p = 24;
@@ -70,14 +65,6 @@ void clc_expand_key_24( clc_aes_key_192 * key ){
 			key->b[p] = key->b[p - 24] ^ buff[a];
 			++p;
 		}
-	}
-}
-
-void clc_init_key_32( clc_aes_key_256 * key, const unsigned char * data, int data_len ){
-	assert(data_len>0 && data_len<=32 && "FAIL: data length must be [1,32] !");
-	clc_init_key(key->b,data,data_len);
-	if(data_len<32){
-		memset(key->b+data_len, 0, 32-data_len );
 	}
 }
 
